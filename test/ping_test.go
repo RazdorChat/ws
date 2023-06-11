38c6ec0dc8f37f1176caf2d723eb95a9314effec
very-amused <very-amused@pm.me>
2023-06-11 14:23:06 -0400

    Test OpText ping

--- test/ping_test.go
+++ test/ping_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestPing(t *testing.T) {
-	t.Run("Ping with OpCode", func(t *testing.T) {
+	t.Run("OpCode Ping", func(t *testing.T) {
 		r.Discard()
 		w.ResetOp(ws.OpPing)
 		_, err := packet.EncodeEvent(w, "ping")
@@ -23,6 +23,30 @@ func TestPing(t *testing.T) {
 		}
 		if header.OpCode != ws.OpPong {
 			t.Errorf("OpPing expects OpPong response, received %s", wsOpCodeName(header.OpCode))
+		}
+		payload, err := packet.Decode(r, header)
+		if err != nil {
+			t.Error(err)
+		}
+		if payload.Event != "pong" {
+			t.Errorf("Pong event expected, received %s", payload.Event)
+		}
+	})
+	t.Run("Text Ping", func(t *testing.T) {
+		r.Discard()
+		w.ResetOp(ws.OpText)
+		_, err := packet.EncodeEvent(w, "ping")
+		if err != nil {
+			t.Error(err)
+		}
+		w.Flush()
+
+		header, err := r.NextFrame()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if header.OpCode != ws.OpText {
+			t.Errorf("OpText expects OpText response, received %s", wsOpCodeName(header.OpCode))
 		}
 		payload, err := packet.Decode(r, header)
 		if err != nil {
